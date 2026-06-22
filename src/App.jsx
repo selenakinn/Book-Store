48ac85c2ebee3f5283493925ab2814e1783406b9
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-06-22 18:09:23 +0300

    Create main App component with routing and context

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+
+import { ProductContextProvider } from './contexts/ProductContext';
+import { CartContextProvider } from './contexts/CartContext';
+
+function App() {
+  return (
+    <ProductContextProvider>
+      <CartContextProvider>
+        <div className="App">
+          <Navigation />
+
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
+
+            <Route path="/cart">
+              <ShoppingCart />
+            </Route>
+          </main>
+        </div>
+      </CartContextProvider>
+    </ProductContextProvider>
+  );
+}
+
+export default App;
